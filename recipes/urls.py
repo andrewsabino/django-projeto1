f0be9abccaa7b22490f6da720a14bac4bb0d032a
Andrew Sabino <andrew.gsabino@hotmail.com>
2022-12-08 11:56:41 -0300

    urls.py

---
+++ recipes/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+
+from recipes.views import contato, home, sobre
+
+urlpatterns = [
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
+]
